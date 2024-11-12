3ce43caaeca530f17102bc2293ba479a3863f48b
simon-schoonjans <simon.schoonjans@waylay.io>
2024-11-12 16:18:32 +0100

    v1.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-logging-loki",
-    version="1.1.1",
+    version="1.2.0",
     description="Python logging handler for Grafana Loki.",
     long_description=long_description,
     long_description_content_type="text/markdown",
